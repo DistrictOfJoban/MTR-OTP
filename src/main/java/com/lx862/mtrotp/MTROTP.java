dddf5c9e0baa0261d6cac906952de3c6104fc05f
LX862 <kenny.mh.hui@outlook.com>
2023-12-29 11:50:28 +0800

    Use elapsed tick for train simulation

--- src/main/java/com/lx862/mtrotp/MTROTP.java
+++ src/main/java/com/lx862/mtrotp/MTROTP.java
@@ -2,15 +2,26 @@ package com.lx862.mtrotp;
 
 import com.lx862.mtrotp.config.Config;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MTROTP implements ClientModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("MTROTP");
-
+    private static float serverTickTime = 1;
     @Override
     public void onInitializeClient() {
         Config.load();
+
+        ServerTickEvents.START_SERVER_TICK.register((server) -> {
+            serverTickTime = ((server.getNextTickTime() - Util.getMillis()) / 50F);
+        });
+
         LOGGER.info("[MTR-OTP] MTR-OTP initialized \\(＾▽＾)/");
+    }
+
+    public static float getServerTickTime() {
+        return serverTickTime;
     }
 }
---
+++ src/main/java/com/lx862/mtrotp/mixin/SidingMixin.java
@@ -0,0 +1,19 @@
+package com.lx862.mtrotp.mixin;
+
+import com.lx862.mtrotp.MTROTP;
+import mtr.data.Siding;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Siding.class)
+public class SidingMixin {
+    @Shadow private Level world;
+
+    @ModifyArg(method = "simulateTrain", at = @At(value = "INVOKE", target = "Lmtr/data/TrainServer;simulateTrain(Lnet/minecraft/world/level/Level;FLmtr/data/Depot;Lmtr/data/DataCache;Ljava/util/List;Ljava/util/Map;Ljava/util/Map;Ljava/util/Map;)Z"), index = 1)
+    public float getTicksElapsed(float ticksElapsed) {
+        return world.isClientSide() ? 1 : MTROTP.getServerTickTime();
+    }
+}
