8ed1e0cf94cde7fa038050d222c0c4a1652e76f0
LX862 <kenny.mh.hui@outlook.com>
2023-12-30 17:33:38 +0800

    Try another approach

--- src/main/java/com/lx862/mtrotp/MTROTP.java
+++ src/main/java/com/lx862/mtrotp/MTROTP.java
@@ -2,23 +2,26 @@ package com.lx862.mtrotp;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MTROTP implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("MTROTP");
-    private static float nextTickTime = -1;
+    private static long last2TickTime = -1;
+    private static long lastTickTime = -1;
     @Override
     public void onInitialize() {
-        ServerTickEvents.START_SERVER_TICK.register((server) -> {
-            nextTickTime = server.getNextTickTime() - Util.getMillis();
+        ServerTickEvents.END_SERVER_TICK.register((server) -> {
+            if(lastTickTime == -1) lastTickTime = System.currentTimeMillis();
+
+            last2TickTime = lastTickTime;
+            lastTickTime = System.currentTimeMillis();
         });
 
         LOGGER.info("[MTR-OTP] MTR-OTP initialized \\(＾▽＾)/");
     }
 
     public static float getNextTickTime() {
-        return nextTickTime == -1 ? 1 : (nextTickTime / 50F);
+        return Math.max(1F, ((lastTickTime - last2TickTime) / 50F));
     }
 }
